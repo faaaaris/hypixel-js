0b41abfded3e0a7e2989b80aa70e85ab5c029f2e
fqriss <bosniangaming626@gmail.com>
2020-05-15 06:16:53 +0200

    Added isGuildId and isUuid

--- util.js
+++ util.js
@@ -3,6 +3,14 @@ const mojang = "https://api.mojang.com";
 
 class Util {
 
+    /**
+     * @description Converts username to uuid
+     * 
+     * @returns {String}
+     * 
+     * @param {name} String - username of player
+     */
+
     nameToUuid(name) {
         await fetch(mojang+"/users/profiles/minecraft/"+name)
         .then(res => {
@@ -13,7 +21,37 @@ class Util {
         .catch(err => {
             throw new Error(err);
         });
-    }
+    };
+
+    /**
+     * @description Checks if the string is a uuid
+     * 
+     * @returns {Boolean}
+     * 
+     * @param {String} uuid - Minecraft player uuid
+     */
+
+    isUuid(uuid) {
+        let f = new RegExp('[0-9a-fA-F]{8}[0-9a-fA-F]{4}[0-9a-fA-F]{4}[0-9a-fA-F]{4}[0-9a-fA-F]{12}');
+        let s = new RegExp('[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}');
+
+        let test = f.test(uuid);
+        let test1 = s.test(uuid);
+
+        return test == false && test1 == false ? false : true;
+    };
+
+    /**
+     * @description Checks if the id is a valid guild id
+     * 
+     * @returns {Boolean}
+     * 
+     * @param {String} id - id of the guild
+     */
+
+    isGuildId(id) {
+        return id.length == 24 ? true : false;
+    };
 }
 
 module.exports = Util;
