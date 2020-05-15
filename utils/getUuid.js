37e412d895fbf92c285c029ea9ae2f15b2c12d52
fqriss <bosniangaming626@gmail.com>
2020-05-15 19:14:58 +0200

    Updated getUuid

--- utils/getUuid.js
+++ utils/getUuid.js
@@ -1,9 +1,9 @@
-const fetch = requrie("node-fetch");
+const fetch = require("node-fetch");
 const url = "https://api.mojang.com";
 
 module.exports = async (query) => {
     await fetch(url+"/users/profiles/minecraft/"+query)
-    .then(res => {
+    .then(async res => {
         if(res.status == 200) {
             const parsed = await res.json();
             return parsed.id;
